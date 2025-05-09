b1dd487400e76ecf20065f6db05b20dede90807a
Balaji <balajich0582gmail.com>
2025-05-09 09:59:04 +0530

    done

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MOG_DB); 
+    await mongoose.connect(process.env.MONGO_DB); 
     console.log('MongoDB Connected');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
